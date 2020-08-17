955da4b6e9fbb972acb3657e75abfbc4aace1bd9
mosaadaldeen <mohammadsd97@gmail.com>
2020-08-17 11:19:09 +0200

    made the frontend of the application

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { detailsProduct } from "../redux/actions/productActions";
+
+function ProductScreen(props) {
+  const [qty, setQty] = useState(1);
+  const productDetails = useSelector((state) => state.productDetails);
+  const { product, loading, error } = productDetails;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(detailsProduct(props.match.params.id));
+  }, []);
+
+  const handleAddToCart = () => {
+    props.history.push(`/cart/${props.match.params.id}?qty=${qty}`);
+  };
+
+  return (
+    <div>
+      <div className="back-to-result">
+        <Link to="/">back to result</Link>
+      </div>
+      {loading ? (
+        <div>Loading ...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <div className="details">
+          <div className="details-image">
+            <img src={product.image} alt="" />
+          </div>
+          <div className="details-info">
+            <ul>
+              <li>
+                <h4>{product.name}</h4>
+              </li>
+              <li>
+                {product.rating} stars ({product.numReviews} reviews)
+              </li>
+              <li>
+                Price: <b>${product.price}</b>
+              </li>
+              <li>
+                Description:
+                <div>{product.description}</div>
+              </li>
+            </ul>
+          </div>
+          <div className="details-action">
+            <ul>
+              <li>price: {product.price}</li>
+              <li>
+                Status: {product.countInStock > 0 ? "in stock" : "unavailable"}
+              </li>
+              <li>
+                Qty:{" "}
+                <select
+                  value={qty}
+                  onChange={(e) => {
+                    setQty(e.target.value);
+                  }}
+                >
+                  {[...Array(product.countInStock).keys()].map((x) => {
+                    return (
+                      <option key={x + 1} value={x + 1}>
+                        {x + 1}
+                      </option>
+                    );
+                  })}
+                </select>
+              </li>
+              <li>
+                {product.countInStock > 0 && (
+                  <button onClick={handleAddToCart} className="button primary">
+                    Add to cart
+                  </button>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+export default ProductScreen;
---
+++ frontend/src/store.js
@@ -0,0 +1,24 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import Cookie from "js-cookie";
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./redux/reducers/productReducers";
+import { cartReducer } from "./redux/reducers/cartReducers";
+
+const cartItems = Cookie.getJSON("cartItems") || [];
+
+const initialState = { cart: { cartItems } };
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
+});
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+);
+export default store;
